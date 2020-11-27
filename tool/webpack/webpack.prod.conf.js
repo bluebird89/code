6816f15eeb4be63b4a209f3d4b438e733c9813bf
Henryli <liboming88@yeah.net>
2020-11-28 00:39:04 +0800

    update

--- tool/webpack/webpack.prod.conf.js
+++ tool/webpack/webpack.prod.conf.js
@@ -4,54 +4,54 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const baseWebpackConfig = require('./webpack.base.conf');
 
 const webpackConfig = merge(baseWebpackConfig, {
-  //environment specific config goes here
-  mode: 'production',
-  devtool: 'source-map',
-  module: {
-    rules: [{
-      test: /\.css$/,
-      use: [
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader',
-          options: {
-            sourceMap: true,
-            modules: true,
-            localIdentName: '[name]---[local]---[hash:base64:5]'
-          }
-        }
-      ]
-    }]
-  },
-  optimization: {
-    runtimeChunk: {
-      "name": "manifest"
+    //environment specific config goes here
+    mode: 'production',
+    devtool: 'source-map',
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true,
+                        modules: true,
+                        localIdentName: '[name]---[local]---[hash:base64:5]'
+                    }
+                }
+            ]
+        }]
     },
-    splitChunks: {
-      cacheGroups: {
-        default: false,
-        vendors: false,
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
+    optimization: {
+        runtimeChunk: {
+            "name": "manifest"
         },
-        styles: {
-          name: 'styles',
-          test: /\.css$/,
-          chunks: 'all',
-          enforce: true
+        splitChunks: {
+            cacheGroups: {
+                default: false,
+                vendors: false,
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                },
+                styles: {
+                    name: 'styles',
+                    test: /\.css$/,
+                    chunks: 'all',
+                    enforce: true
+                }
+            }
         }
-      }
-    }
-  },
-  plugins: [
-    new webpack.HashedModuleIdsPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
-      chunkFilename: "[name].[contenthash].css"
-    })
-  ]
+    },
+    plugins: [
+        new webpack.HashedModuleIdsPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css",
+            chunkFilename: "[name].[contenthash].css"
+        })
+    ]
 });
 
 module.exports = webpackConfig;
