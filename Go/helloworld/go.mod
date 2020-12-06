d7ae8d7e27f0df66d637ae8d5b6302f4e76040f1
Henryli <liboming88@yeah.net>
2020-12-06 15:54:02 +0800

    tidy

--- Go/helloworld/go.mod
+++ Go/helloworld/go.mod
@@ -4,4 +4,7 @@ go 1.14
 
 replace google.golang.org/grpc => github.com/grpc/grpc-go v1.29.1
 
-require google.golang.org/grpc v1.25.1
+require (
+	google.golang.org/grpc v1.25.1
+	rsc.io/quote v1.5.2 // indirect
+)
--- Go/helloworld/hello.go
+++ Go/helloworld/hello.go
@@ -1,1 +1,12 @@
 package main
+
+import "fmt"
+import "rsc.io/quote"
+import "./geetings"
+
+func main(){
+	fmt.Println("Hello World!")
+	fmt.Println(quote.Go())
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
+}
