b55348ede2550593447196b26eef843e42f0bf3a
henry <liboming88@yeah.net>
2021-01-04 19:34:50 +0800

    fix go env

--- Go/syntax/src/go.mod
+++ Go/syntax/src/go.mod
@@ -1,9 +1,3 @@
-module syntax
+module example.com/syntax
 
 go 1.15
-
-require (
-	github.com/gofrs/uuid v3.3.0+incompatible
-	github.com/jinzhu/gorm v1.9.16
-	github.com/spaolacci/murmur3 v1.1.0
-)
--- Go/syntax/src/httpServer.go
+++ Go/syntax/src/httpServer.go
@@ -1,19 +1,67 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
+type HelloHandlerStruct struct {
+	content string
+}
+
+func (handler *HelloHandlerStruct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, handler.content)
+}
+
+type WelcomeHandlerStruct struct {
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+
+func (*WelcomeHandlerStruct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome")
+}
+
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", HelloHandler)
+	mux.Handle("/welcome", &WelcomeHandlerStruct{})
+
 	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
 		params := request.URL.Query()
 		fmt.Fprintf(writer, "你好, %s", params.Get("name"))
 	})
+	http.Handle("/", &HelloHandlerStruct{content: "Hello World"})
 
-	err := http.ListenAndServe(":8080", nil)
+	server := &http.Server{
+		Addr:    ":8081",
+		Handler: mux,
+	}
+	// 创建系统信号接收器
+	done := make(chan os.Signal)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-done
+
+		if err := server.Shutdown(context.Background()); err != nil {
+			log.Fatal("Shutdown server:", err)
+		}
+	}()
+
+	log.Println("Starting HTTP server...")
+	err := server.ListenAndServe()
 	if err != nil {
-		log.Fatalf("启动 HTTP 服务器失败: %v", err)
+		if err == http.ErrServerClosed {
+			log.Print("Server closed under request")
+		} else {
+			log.Fatal("Server closed unexpected")
+		}
 	}
 }
