e11cb621585dfd017154c2d7e17714446ff376c4
henry <liboming88@yeah.net>
2021-01-18 11:45:01 +0800

    go:add pattern

---
+++ Go/design_pattern/employee_filter.go
@@ -0,0 +1,22 @@
+package gen
+
+import "fmt"
+
+type EmployeeList []Employee
+type EmployeeToBool func(*Employee) bool
+func (al EmployeeList) Filter(f EmployeeToBool) EmployeeList {
+	var ret EmployeeList
+	for _, a := range al {
+		if f(&a) {
+			ret = append(ret, a)
+		}
+	}
+	return ret
+}
+
+type Employee struct {
+	Name     string
+	Age      int
+	Vacation int
+	Salary   int
+}
